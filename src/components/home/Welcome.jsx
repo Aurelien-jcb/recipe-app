060c9e161b1da66018041f092785430ba384b00c
Aurelien jacob <jacob-aurelien@hotmail.fr>
2021-07-16 12:15:31 +0200

    add icons

--- src/components/home/Welcome.jsx
+++ src/components/home/Welcome.jsx
@@ -1,20 +1,23 @@
 import { Typography } from "@material-ui/core";
 import { Grid, makeStyles } from "@material-ui/core";
 import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
 import { UserContext } from "../../storage/context/userContext";
 
 const useStyles = makeStyles((theme) => ({
-    appLogo: {
-        margin: '0 0 40px 0'
-    }
+  appLogo: {
+    margin: "0 0 40px 0",
+  },
 }));
 export default function Welcome() {
   const classes = useStyles();
-  const {userContext } = useContext(UserContext);
+  const { userContext } = useContext(UserContext);
   return (
     <>
       <Grid container item xs={12} className={classes.appLogo}>
-        Logo
+        <NavLink to="/" className={classes.navLink}>
+          <img onClick="/" src="/assets/img/cookedIn-logo.png" />
+        </NavLink>
       </Grid>
       <Grid container item xs={12}>
         <Grid container item xs={12}>
